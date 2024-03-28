ede5947e5df5f7901978c21c788538639675db91
Dez101 <dkay1101@gmail.com>
2024-03-28 22:32:54 +0200

    Add License

--- Pages/Licenses.cshtml.cs
+++ Pages/Licenses.cshtml.cs
@@ -1,6 +1,7 @@
 using CryptlexLicensingApp.Models;
 using CryptlexLicensingApp.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
@@ -11,24 +12,45 @@ namespace CryptlexLicensingApp.Pages
         private readonly ILogger<LoginModel> _logger = logger;
         private readonly HttpService _httpService = httpService;
         public List<License> Licences { get; set; } = [];
+        public List<ProductResponse> ProductResponses { get; set; } = [];
+
+        [BindProperty]
+        public string ProductId { get; set; }
 
         public async Task OnGet()
         {
             try
             {
+                await GetProducts();
                 await GetLicenses();
             }
             catch (Exception ex)
             {
-                Console.Write(ex);
                 throw;
             }
+        }
+
+        private async Task GetProducts()
+        {
+            ProductResponses = await _httpService.SendGetAsync<List<ProductResponse>>("v3/products");
+            _logger.LogInformation("Products: {Count}", ProductResponses?.Count);
         }
 
         private async Task GetLicenses()
         {
             Licences = await _httpService.SendGetAsync<List<License>>("v3/licenses");
             _logger.LogInformation("Licenses: {Count}", Licences.Count);
+        }
+
+        public async Task OnPost()
+        {
+            var data = new
+            {
+                ProductId,
+            };
+
+            var rtn = await _httpService.SendPostAsync<ProductPostResponse>("v3/licenses", data);
+            HttpContext.Response.Redirect("/licenses");
         }
     }
 }
